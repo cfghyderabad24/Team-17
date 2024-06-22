30cba1ccc2466b8be8ccd5b2632cfaf2d64f5158
Anirudh1506 <2100031630cseh@gmail.com>
2024-06-22 15:15:29 +0530

    Second commit

--- backend/Teacher/views.py
+++ backend/Teacher/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Teacher
+from .forms import TeacherForm
 # Create your views here.
+
+def createTeacher(request):
+    form=TeacherForm()
+
+    if request.method=='POST':
+        form=TeacherForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+    return render(request,'teacher_form.html',{'form':form})
+
+def getTeacher(request):
+    teacher=Teacher.objects.all()
+    return render(request,'homepage.html',{'teachers':teacher})
